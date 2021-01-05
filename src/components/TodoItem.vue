9b9f629013abb203d23062c5946cb9a957c89e62
Edis Gonuler <edis@gonuler.com>
2021-01-04 19:20:11 -0500

    add todo item delete and complete functionality

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,19 +1,49 @@
 <template>
-  <div>
+  <div class="ui segment">
     <div class="ui checkbox">
-      <input type="checkbox" name="example" />
-      <label>{{ content }}</label>
+      <!-- TODO: combine lines into one -->
+
+      <div v-if="completed === true">
+        <input type="checkbox" @click='$emit("on-toggle")' checked />
+        <label class="completed">{{ content }}</label>
+      </div>
+      <div v-else>
+        <input type="checkbox" @click='$emit("on-toggle")'/>
+        <label>{{ content }}</label>
+      </div>
     </div>
+
+    <button class="ui icon negative button" @click='$emit("on-delete")'>
+      <i class="trash icon" />
+    </button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'TodoItem',
-  props: ['content'],
+  props: {
+    content: String,
+    completed: Boolean,
+  },
 };
 </script>
 
 <style scoped>
+.segment {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  text-align: left;
+  padding-left: 1rem;
+  padding-right: 1rem;
+}
 
+.completed {
+  text-decoration: line-through;
+}
+
+.button {
+  font-size: 0.75rem;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,22 +3,64 @@
     <h1>To-do app using Vue.js</h1>
 
     <div class="ui container">
-      <AddTodo />
+      <form class="ui form" @submit.prevent="addTodo()">
+        <div class="fields">
+          <div class="twelve wide field">
+            <input v-model="newTodo" type="text" name="todo-content"
+                   placeholder="What do you want to finish ..." />
+          </div>
+          <div class="four wide field">
+            <button class="ui primary button">Add Todo</button>
+          </div>
+        </div>
+      </form>
+
       <h4 class="ui dividing header" />
-      <TodoItem content="hello"/>
+
+      <div class="ui segments">
+        <TodoItem v-for="(todo, index) in todos" :key="index"
+                  :content="todo.content" :completed="todo.completed"
+                  @on-toggle="toggleTodo(todo)" @on-delete="deleteTodo(todo)"/>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import TodoItem from '@/components/TodoItem.vue';
-import AddTodo from '@/components/AddTodo.vue';
 
 export default {
   name: 'Home',
-  components: {
-    AddTodo,
-    TodoItem,
+  components: { TodoItem },
+  data() {
+    return {
+      todos: [
+        { content: 'Learn Vue.js', completed: true },
+        { content: 'Work on Vue.js Project', completed: false },
+      ],
+      newTodo: '',
+    };
+  },
+
+  methods: {
+    addTodo() {
+      this.todos.push({
+        content: this.newTodo,
+        completed: false,
+      });
+
+      this.newTodo = '';
+    },
+
+    toggleTodo(todo) {
+      // eslint-disable-next-line no-param-reassign
+      todo.completed = !todo.completed;
+    },
+
+    deleteTodo(todo) {
+      const index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
   },
 };
 </script>
@@ -26,5 +68,8 @@ export default {
 <style>
   h1 {
     margin-bottom: 2rem;
+  }
+  label {
+    text-align: left;
   }
 </style>
